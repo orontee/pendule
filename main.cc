0efb3e6cd1a54965cfaff3894746404f028721a5
Matthias Meulien <orontee@gmail.com>
2026-01-17 16:46:34 +0100

    CLI argument to start fullscreen

--- main.cc
+++ main.cc
@@ -1,26 +1,51 @@
-#include "clock.h"
+#include <getopt.h>
 #include <gtkmm/application.h>
 #include <gtkmm/window.h>
 
-class PenduleWindow : public Gtk::Window
-{
-public:
-  PenduleWindow();
+#include "clock.h"
 
-protected:
-  Clock m_clock;
-};
+namespace {
+bool fullscreen = false;
 
-PenduleWindow::PenduleWindow()
-{
-  set_title("Pendule");
-  set_child(m_clock);
-  fullscreen();
+void check_fullscreen_argument(int argc, char *argv[]) {
+  int opt;
+
+  static struct option long_options[] = {
+    {"fullscreen", no_argument, 0, 'f'},
+    {nullptr, 0, nullptr, 0}};
+  const char *short_options = "f";
+
+  while ((opt = getopt_long(argc, argv, short_options, long_options, nullptr)) != -1) {
+    if (opt == 'f') {
+      fullscreen = true;
+      break;
+    }
+  }
 }
 
-int main(int argc, char** argv)
-{
+class PenduleWindow : public Gtk::Window
+  {
+  public:
+    PenduleWindow() {
+      set_title("Pendule");
+      set_child(this->clock);
+      if (::fullscreen) {
+        this->fullscreen();
+      }
+    }
+
+protected:
+  Clock clock;
+};
+}
+
+int main(int argc, char **argv) {
+  check_fullscreen_argument(argc, argv);
+
   auto app = Gtk::Application::create("io.github.orontee.pendule");
 
-  return app->make_window_and_run<PenduleWindow>(argc, argv);
+  if (fullscreen) {
+    argc = 1;                   // to hide option
+  }
+  return app->make_window_and_run<::PenduleWindow>(argc, argv);
 }
